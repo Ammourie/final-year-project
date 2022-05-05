6721125a36ace4cf51164db12e02fcfd58afccb1
Mohammed Al-Ammourie <mohammed.amory.it@gmail.com>
2022-05-05 17:50:44 +0300

    ttt

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,15 +1,16 @@
+
 import { Component, OnInit } from '@angular/core';
+
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.css'],
 })
+
 export class StudentsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
